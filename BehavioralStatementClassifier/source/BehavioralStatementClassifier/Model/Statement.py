33de6e38a502eee3b3c3e6b951c6389839cb2b73
PerformanceAllyLLC <tom.donaldson@performanceally.com>
2021-09-12 15:53:20 -0400

    Creating statement class

---
+++ BehavioralStatementClassifier/source/BehavioralStatementClassifier/Model/Statement.py
@@ -0,0 +1,86 @@
+# ##############################################################################
+#
+#  File: Statement.py
+#
+#  Last Modified: 9/12/21, 3:42 PM
+#
+#  By: tompadonaldson
+#
+#  Project: BehavioralStatementClassifier
+#
+#  Project Purpose: The purpose of the software is to collect and evaluate
+#  statements of behavior as part of identifying behaviors important in
+#  producing some result, otherwise known as, "pinpointing vital behaviors".
+#  Such statements would be specific to particular domains and perhaps settings
+#  within those domains. Besides the general need of classifying behaviors, we
+#  also want to collect data that will support efforts to improve the entire
+#  process: train users to classify statements; the criteria used in
+#  classifying statements; the explanations of the criteria; how the user
+#  interacts with the system; and so on.
+#
+#  File Purpose: <enter comments here>
+#
+#  Copyright © Performance Ally, LLC, 2021.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# ##############################################################################
+
+# Built in Python types for "hinting"
+from typing import List, AnyStr, Any, Dict
+
+
+class Statement:
+    """
+    Holds one textual statement containing at least one character, identified
+    by a numeric id.
+
+    The numeric id allows the statement to be referenced from various
+    locations, and to be held in tables, in a consistent manner, and to be
+    accessed by a simple id.
+    """
+
+    # ##########################################################################
+    # STATIC METHODS
+    # ##########################################################################
+
+    # ##########################################################################
+    # PROPERTIES
+    # ##########################################################################
+
+    @property
+    def id(self) -> int:
+        return self._id
+
+    @property
+    def text(self) -> AnyStr:
+        return self._text
+
+    # ##########################################################################
+    # CONSTRUCTOR
+    # ##########################################################################
+
+    def __init__(self, id: int, text: AnyStr):
+        assert id >= 0
+        assert text is not None
+        assert len(text) > 0
+
+        self._id = id
+        self._text = text
+
+    # ##########################################################################
+    # METHODS
+    # ##########################################################################
+
+    # ##########################################################################
+    # PROTECTED DATA
+    # ##########################################################################
+
+    _id: int
+    _text: AnyStr
+
+
---
+++ BehavioralStatementClassifier/source/BehavioralStatementClassifier/Model/tests/test_Statement.py
@@ -0,0 +1,38 @@
+# ##############################################################################
+#
+#  File: test_Statement.py
+#
+#  Last Modified: 9/12/21, 3:51 PM
+#
+#  By: tompadonaldson
+#
+#  Project: BehavioralStatementClassifier
+#
+#  Project Purpose: The purpose of the software is to collect and evaluate
+#  statements of behavior as part of identifying behaviors important in
+#  producing some result, otherwise known as, "pinpointing vital behaviors".
+#  Such statements would be specific to particular domains and perhaps settings
+#  within those domains. Besides the general need of classifying behaviors, we
+#  also want to collect data that will support efforts to improve the entire
+#  process: train users to classify statements; the criteria used in
+#  classifying statements; the explanations of the criteria; how the user
+#  interacts with the system; and so on.
+#
+#  File Purpose: <enter comments here>
+#
+#  Copyright © Performance Ally, LLC, 2021.
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+# ##############################################################################
+
+
+
+
+
+class TestStatement:
+    pass
