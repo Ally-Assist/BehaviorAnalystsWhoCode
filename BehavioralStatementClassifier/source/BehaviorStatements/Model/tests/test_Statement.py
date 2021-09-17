2e7fe82050f830f3ea8b5636ca356ac295e8dd06
PerformanceAllyLLC <tom.donaldson@performanceally.com>
2021-09-17 14:47:13 -0400

    Update test_Statement.py

--- BehavioralStatementClassifier/source/BehaviorStatements/Model/tests/test_Statement.py
+++ BehavioralStatementClassifier/source/BehaviorStatements/Model/tests/test_Statement.py
@@ -2,32 +2,6 @@
 #
 #  File: test_Statement.py
 #
-#  Last Modified: 9/17/21, 6:28 AM
-#
-#  By: tompadonaldson
-#
-#  Project: BehavioralStatementClassifier
-#
-#  Project Purpose: The purpose of the software is to collect and evaluate
-#  statements of behavior as part of identifying behaviors important in
-#  producing some result, otherwise known as, "pinpointing vital behaviors".
-#
-#  File Purpose: <enter comments here>
-#
-#  Copyright © Performance Ally, LLC, 2021.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#         http://www.apache.org/licenses/LICENSE-2.0
-#
-# ##############################################################################
-
-# ##############################################################################
-#
-#  File: test_Statement.py
-#
 #  Last Modified: 9/12/21, 3:51 PM
 #
 #  By: tompadonaldson
@@ -44,7 +18,7 @@
 #  classifying statements; the explanations of the criteria; how the user
 #  interacts with the system; and so on.
 #
-#  File Purpose: <enter comments here>
+#  File Purpose: Isolated tests for the Statement class.
 #
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
